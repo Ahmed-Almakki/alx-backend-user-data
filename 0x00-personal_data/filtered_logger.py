8924240af378e3e93165d119c1e7f020736a487a
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-11-07 18:47:37 +0000

    other way regx

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,17 +5,13 @@ from typing import List
 import logging
 
 
-patterns = {
-    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
-    'replace': lambda x: r'\g<field>={}'.format(x),
-}
-
-
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
-    """ search for fields and changge it to unreadabl formate"""
-    extract, replace = (patterns["extract"], patterns["replace"])
-    return re.sub(extract(fields, separator), replace(redaction), message)
+    """ Replacing """
+    for f in fields:
+        message = re.sub(rf"{f}=(.*?)\{separator}",
+                         f'{f}={redaction}{separator}', message)
+    return message
 
 
 class RedactingFormatter(logging.Formatter):
@@ -33,9 +29,5 @@ class RedactingFormatter(logging.Formatter):
 
     def format(self, record: logging.LogRecord) -> str:
         """ formats a LogRecord"""
-        msg = super(RedactingFormatter, self).format(record)
-        txt = filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
-        return txt
-
-if __name__ == "__main__":
-    main()
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
