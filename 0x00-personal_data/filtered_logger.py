eda19efdbf7eea15873fa388a540b3b0dcbbd05e
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-11-07 15:39:35 +0000

    use format print

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,7 +7,6 @@ from typing import List
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """ search for fields and changge it to unreadabl formate"""
-    for fl in fields:
-        message = re.sub(r"(?<=" + re.escape(fl) + r"=)[^" + separator + r"]+",
-                         redaction, message)
+    for f in fields:
+        message = re.sub(fr"(?<={f}=)[^{separator}]+", redaction, message)
     return message
