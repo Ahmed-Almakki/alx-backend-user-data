4691b2dccf789a9e1be77e272c8ed19bf1670fb1
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-11-07 23:44:47 +0000

    logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,5 @@
 #!/usr/bin/evn python3
-""" Regex-ing """
+""" loggin file that contain obfuscated data """
 import re
 from typing import List
 import logging
@@ -7,7 +7,7 @@ import logging
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
-    """ Replacing """
+    """ Replacing certen filed with obfuscated data """
     for f in fields:
         message = re.sub(rf"{f}=(.*?)\{separator}",
                          f'{f}={redaction}{separator}', message)
@@ -23,11 +23,23 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        """ Initilaizing"""
+        """ Initilaizing with field paramter"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """ formats a LogRecord"""
+        """ formats a LogRecord using filter function"""
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """ createing a logger file """
+    logger = logging.getLogger("user_data")
+    logger.propagate = False
+    logger.setLevel(logging.INFO)
+
+    st = logging.StreamHandler()
+    st.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(st)
+    return logger
