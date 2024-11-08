03f5e15665df33258d26fea997efbd30a0645859
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-11-08 00:46:19 +0000

    other way

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,15 +1,21 @@
-#!/usr/bin/evn python3
-""" loggin file that contain obfuscated data """
+#!/usr/bin/env python3
+"""
+Personal data
+"""
+
+
+import logging
+import os
 import re
 from typing import List
-import logging
 import mysql.connector
-import os
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
-    """ Replacing certen filed with obfuscated data """
+    """ Replacing """
     for f in fields:
         message = re.sub(rf"{f}=(.*?)\{separator}",
                          f'{f}={redaction}{separator}', message)
@@ -17,60 +23,65 @@ def filter_datum(fields: List[str], redaction: str, message: str,
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
+    """ RedactingFormatter class. """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        """ Initilaizing with field paramter"""
-        super(RedactingFormatter, self).__init__(self.FORMAT)
+        """ Init """
         self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
 
     def format(self, record: logging.LogRecord) -> str:
-        """ formats a LogRecord using filter function"""
+        """ Format """
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), self.SEPARATOR)
 
 
 def get_logger() -> logging.Logger:
-    """ createing a logger file """
-    logger = logging.getLogger("user_data")
-    logger.propagate = False
-    logger.setLevel(logging.INFO)
+    """ Implementing a logger.
+    """
 
-    st = logging.StreamHandler()
-    st.setFormatter(RedactingFormatter(PII_FIELDS))
-    logger.addHandler(st)
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(handler)
     return logger
 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
-    """ connect to database using enviromental variable"""
-    user = os.environ.get("PERSONAL_DATA_DB_USERNAME")
-    password = os.environ.get("PERSONAL_DATA_DB_PASSWORD")
-    host = os.environ.get("PERSONAL_DATA_DB_HOST")
-    db = os.environ.get("PERSONAL_DATA_DB_NAME")
-    connct = mysql.connector.connect(user=user, password=password,
-                                     host=host, database=db)
-    return connct
+    """ Implement db conectivity
+    """
+    psw = os.environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    username = os.environ.get('PERSONAL_DATA_DB_USERNAME', "root")
+    host = os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost')
+    db_name = os.environ.get('PERSONAL_DATA_DB_NAME')
+    conn = mysql.connector.connect(
+        host=host,
+        database=db_name,
+        user=username,
+        password=psw)
+    return conn
 
 
 def main() -> None:
-    """main function to run"""
+    """ Implement a main function
+    """
     db = get_db()
     cursor = db.cursor()
     cursor.execute("SELECT * FROM users;")
     for row in cursor:
-        message = f"name{row[0]}; email={row[1]}; phone={row[2]}; "+\
-                f"ssn={row[3]}; password={row[4]};ip={row[5]}; " +\
-                f"last_login={row[6]}; user_agent={row[7]};"
+        message = f"name={row[0]}; email={row[1]}; phone={row[2]}; " +\
+            f"ssn={row[3]}; password={row[4]};ip={row[5]}; " +\
+            f"last_login={row[6]}; user_agent={row[7]};"
         print(message)
     cursor.close()
     db.close()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
