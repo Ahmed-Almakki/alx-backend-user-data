8838f9283e24801c3500a026c349b6541436117d
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-11-08 01:04:23 +0000

    check if is true password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,12 @@ def hash_password(password: str) -> bytes:
     pas = str.encode(password)
     hashed = bcrypt.hashpw(pas, bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Implement an is_valid function that expects 2 argument"""
+    pas = str.encode(password)
+    if bcrypt.checkpw(pas, hashed_password):
+        return True
+    else:
+        return False
