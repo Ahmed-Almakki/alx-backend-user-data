bfd2ae33649cca92c8d962777068c6bd48b773a4
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-11-13 00:14:07 +0000

    decode and encode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Basic Authentication class """
 from api.v1.auth.auth import Auth
+import base64
 import re
 
 
@@ -18,3 +19,19 @@ class BasicAuth(Auth):
             return None
         spn = re.search(r'(?<=Basic ).*', authorization_header).span()
         return authorization_header[spn[0]: spn[1]]
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """ decode a header using base64
+        """
+        if base64_authorization_header is None\
+                or not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            data = base64_authorization_header.encode('utf-8')
+
+            dcd_data = base64.b64decode(data).decode('utf-8')
+            return dcd_data
+        except Exception:
+            return None
