9206d27d9c8c64a07d9f9e0e10f64d2e8d1aa21b
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-11-14 21:20:03 +0000

    without importing other dependencies

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,6 @@
 """class SessionAuth that inherits from Auth"""
 from api.v1.auth.auth import Auth
 from typing import List, TypeVar
-from models.user import User
-from uuid import uuid4
 
 
 class SessionAuth(Auth):
