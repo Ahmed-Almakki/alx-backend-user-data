408940df752a60c994c0204cfd704b4bb6e1afd6
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-11-15 00:36:14 +0000

    current user

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 from api.v1.auth.auth import Auth
 from typing import List, TypeVar
 from uuid import uuid4
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -22,3 +23,13 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ return user user id"""
+        session_id = self.session_cookie(request)
+        if not session_id:
+            return
+        user_id = self.user_id_for_session_id(session_id)
+        if not user_id:
+            return
+        return User.get(user_id)
