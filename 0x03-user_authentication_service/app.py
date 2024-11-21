dadd1d2b394fd1acf30818a877f7a22fed2840c0
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-11-21 22:13:31 +0000

    check if there is session firstly

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -44,6 +44,8 @@ def login():
 def logout():
     """ logout from session"""
     session_id = request.cookies.get("session_id")
+    if session_id in None:
+        abort(403)
     user = AUTH.get_user_from_session_id(session_id)
     if user is not None:
         AUTH.destroy_session(user.id)
