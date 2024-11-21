b842b0325d99e8b41234df5f0d27c75480bd6cf9
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-11-21 21:57:40 +0000

    get fom cookie

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -40,7 +40,7 @@ def login():
 
 @app.route("/sessions", methods=['DELETE'])
 def logout():
-    session_id = request.form.get("session_id")
+    session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
     if user:
         AUTH.destroy_session(user.id)
