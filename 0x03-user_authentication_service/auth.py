7f6a497b9a08fce46e2f90c503181dd06405b24d
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-11-21 00:12:50 +0000

    checked if register

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,8 @@
 Authentication file
 """
 import bcrypt
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -10,3 +12,24 @@ def _hash_password(password: str) -> bytes:
     byts = password.encode('utf-8')
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(byts, salt)
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ register user in database if not exists"""
+        chk = True
+        try:
+            user = self._db.find_user_by(email=email)
+        except Exception:
+            chk = False
+        if chk:
+            raise ValueError("User {} already Exists".format(email))
+        hased = _hash_password(password)
+        user = self._db.add_user(email, hased)
+        return user
